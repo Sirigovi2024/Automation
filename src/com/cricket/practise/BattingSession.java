5eb43462007545bed7c7228cc96a08dc2c45dfa4
Sirigovi2024 <sirivellagovindamma15@gmail.com>
2024-02-12 11:04:49 +0530

    Automation

---
+++ src/com/cricket/practise/BattingSession.java
@@ -0,0 +1,11 @@
+package com.cricket.practise;
+
+public class BattingSession {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello RCBians!!!!");
+
+	}
+
+}
---
+++ src/com/javalearn/returntypes/ReturnInt.java
@@ -0,0 +1,49 @@
+package com.javalearn.returntypes;
+
+public class ReturnInt {
+
+	public static void main(String[] args) {
+		/*nulmethod();
+	    int res = intmethod();
+	    System.out.println(res);
+		//System.out.println(intmethod());
+		int total =charMethod();
+		System.out.println(charMethod());
+		int a ='x';
+		System.out.println("Value of a is " +a);
+		String b ="121";
+		System.out.println(b);*/
+		divmethod();
+		
+		}
+  public static void divmethod()
+  {
+	  int a = 10;
+	  int b =20;
+	  float c =a/b;
+	  System.out.println(c);
+	   
+	  }
+  /*public static int intmethod() {
+  int a =50;
+  int b =100;
+  int c = a+b; 
+  //System.out.println(c);
+  return c;
+  }*/
+  public static int charMethod()
+  {
+	  char a ='x';
+	  int z= 50;
+	 return a;
+	   }
+  public static void add() {
+	  int x =10;
+	  //return x;
+	  }
+  
+
+ 
+	  
+  }
+
